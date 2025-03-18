821933f43bc31640f33a4c027bd09e342ea0d96c
Mavliut Fozliev <mavliut.fozliev@gmail.com>
2025-03-18 21:49:10 +0300

    update metro config

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,15 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
+const defaultConfig = getDefaultConfig(__dirname);
+
 const config = {
   transformer: {
-    assetPlugins: ['react-native-svg-transformer'],
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
   },
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(defaultConfig, config);
