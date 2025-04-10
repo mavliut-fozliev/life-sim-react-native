79d1306c794efac3f8ec5a979f6a2872df2620e2
Mavliut Fozliev <mavliut.fozliev@gmail.com>
2025-04-11 01:15:37 +0300

    feat: add character images

--- src/UI/pages/Menu/src/Home/src/sections/visualization/Visualization/Visualization.tsx
+++ src/UI/pages/Menu/src/Home/src/sections/visualization/Visualization/Visualization.tsx
@@ -4,8 +4,32 @@ import {Image, StyleSheet, View} from 'react-native';
 function Visualization() {
   return (
     <View style={styles.box}>
-      <Image source={require('../../../../../../../../../assets/images/face3.png')} style={styles.face} />
-      <Image source={require('../../../../../../../../../assets/images/eyes2.png')} style={styles.eyes} />
+      <View style={styles.character}>
+        <Image
+          source={require('../../../../../../../../../assets/images/character/body/body_2.png')}
+          style={styles.image}
+        />
+        <Image
+          source={require('../../../../../../../../../assets/images/character/legs/legs.png')}
+          style={styles.image}
+        />
+        <Image
+          source={require('../../../../../../../../../assets/images/character/head/head_2.png')}
+          style={styles.image}
+        />
+        <Image
+          source={require('../../../../../../../../../assets/images/character/hair/hair_1.png')}
+          style={styles.image}
+        />
+        <Image
+          source={require('../../../../../../../../../assets/images/character/eyes/eyes.png')}
+          style={styles.image}
+        />
+        <Image
+          source={require('../../../../../../../../../assets/images/character/mouth/mouth.png')}
+          style={styles.image}
+        />
+      </View>
     </View>
   );
 }
@@ -15,19 +39,15 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0,0,255,0.2)',
     height: 300,
   },
-  face: {
-    width: 100,
-    height: 100,
-    position: 'absolute',
-    top: 0,
-    left: 0,
+  character: {
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    height: '100%',
   },
-  eyes: {
-    width: 100,
-    height: 100,
+  image: {
+    width: 150,
+    height: 150,
     position: 'absolute',
-    top: 10,
-    left: 0,
   },
 });
 
