0eebf918676827eeabd619d90e8709538337be4d
Mavliut Fozliev <mavliut.fozliev@gmail.com>
2025-04-17 23:33:30 +0300

    feat: add surnames

---
+++ src/consts/characterSurnames.ts
@@ -0,0 +1,36 @@
+import {Countries} from './countries';
+
+export const characterSurnames = {
+  [Countries.ALB]: {
+    Male: ['Berisha', 'Hoxha', 'Krasniqi', 'Shehu', 'Hasani', 'Leka', 'Dervishi', 'Shala', 'Gashi', 'Islami'],
+    Female: ['Berisha', 'Hoxha', 'Krasniqi', 'Shehu', 'Hasani', 'Leka', 'Dervishi', 'Shala', 'Gashi', 'Islami'],
+  },
+  [Countries.RUS]: {
+    Male: [
+      'Ivanov',
+      'Smirnov',
+      'Kuznetsov',
+      'Popov',
+      'Vasiliev',
+      'Petrov',
+      'Sokolov',
+      'Mikhailov',
+      'Novikov',
+      'Fedorov',
+    ],
+    Female: [
+      'Ivanova',
+      'Smirnova',
+      'Kuznetsova',
+      'Popova',
+      'Vasilieva',
+      'Petrova',
+      'Sokolova',
+      'Mikhailova',
+      'Novikova',
+      'Fedorova',
+    ],
+  },
+  [Countries.TUR]: {Male: ['Erion'], Female: ['Erion']},
+  [Countries.USA]: {Male: ['Erion'], Female: ['Erion']},
+};
--- src/locales/ru/index.ts
+++ src/locales/ru/index.ts
@@ -2,10 +2,12 @@ import common from './common';
 import menu from './menu';
 import places from './places';
 import characterNames from './characterNames';
+import characterSurnames from './characterSurnames';
 
 export default {
   common,
   menu,
   places,
   characterNames,
+  characterSurnames,
 };
